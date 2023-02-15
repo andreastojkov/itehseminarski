cd33a9e35fb82ace7c39d742f4e445319098bea2
Andrea Stojkov <andreastojkov00@gmail.com>
2023-02-15 14:18:54 +0100

    Instalirane stavke, napravljena Login stranica

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import './index.css'
+import {createApp} from 'vue'
+import store from './store'
+import router from './router'
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app')
